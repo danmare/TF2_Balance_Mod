c4e780ef764e04b63a633c34c438f2adc9ef88f0
danmare <66508100+danmare@users.noreply.github.com>
2025-09-02 01:44:57 -0400

    Custom Items Progress #2

--- src/game/shared/econ/econ_item_schema.cpp
+++ src/game/shared/econ/econ_item_schema.cpp
@@ -4423,7 +4423,7 @@ bool CEconItemSchema::BInitTextBuffer( CUtlBuffer &buffer, CUtlVector<CUtlString
 	Reset();
 	m_pKVRawDefinition = new KeyValues( "CEconItemSchema" );
 	//if ( m_pKVRawDefinition->LoadFromBuffer( NULL, buffer ) ) //@@//
-	if (m_pKVRawDefinition->LoadFromFile(g_pFullFileSystem, "mod_tf/scripts/items/items_custom.txt", "GAME")) //??//
+	if (m_pKVRawDefinition->LoadFromFile(g_pFullFileSystem, "scripts/items/items_custom.txt", "GAME")) //??//
 	{ //@@//
 		return BInitSchema( m_pKVRawDefinition, pVecErrors )
 			&& BPostSchemaInit( pVecErrors );
